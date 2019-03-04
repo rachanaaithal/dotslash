7524e88ca8c5f1829eb0ab1ee682eb4da0d4f9d8
Bharath Vedartham <linux.bhar@gmail.com>
2019-03-04 11:05:28 +0530

    init_firebase: Added firebase config files
    Added firebase config.
    
    Signed-off-by: Bharath Vedartham <linux.bhar@gmail.com>

---
+++ init_firebase.py
@@ -0,0 +1,8 @@
+import pyrebase
+
+config = {
+        "apiKey": "AIzaSyAo1tFyg-y8BfMdpEca4YJ3n0U_s_2R4-I",
+        "authDomain": "dotslash-e3ae8.firebaseapp.com",
+        "databaseURL": "https://dotslash-e3ae8.firebaseio.com/",
+        "storageBucket": "dotslash-e3ae8.appspot.com"
+        }
