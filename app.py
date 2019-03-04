95de1ce5ae2a682feacf880ba83ee778f459adc8
Bharath Vedartham <linux.bhar@gmail.com>
2019-03-04 16:15:53 +0530

    Added update methods and integrated with flask server.
    Signed-off-by: Bharath Vedartham <linux.bhar@gmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, render_template
-from firebase_db import db, get_request_data
+from firebase_db import db, get_request_data, update_data
 from firebase_storage import storage
 import json
 
@@ -13,7 +13,9 @@ def hello_world():
 
 @app.route("/requests",methods = ['GET'])
 def get_requests():
-
+	'''
+	This function gets all the queries and returns them as a json object.
+	'''
 	if request.method == 'GET':
 		data = get_request_data("queries").val()
 		response = {}
@@ -23,12 +25,40 @@ def get_requests():
 			val = v
 			response[key] = val
 
-		return jsonify(response), 201
+		if response:
+			return jsonify(response), 201
+		else:
+			return jsonify({}), 401
 
 	else:
 		return jsonify({}), 405
 
+@app.route("/send_string", methods = ['POST'])
+def update_response():
+	'''	
+	This route updates the response string typed by the volunteer. 
+	A json request with following structure:
+	{
+		"Id" : Key of the json object
+		"Response string": Response string
+	}
+	'''
+	if request.method == 'POST':
+		try:
+			id_tag = request.json['id']
+			response_string = request.json['response']
+		except KeyError:
+			return jsonify({}),401
+
+		tag = "queries"
+		retval = update_data(id_tag, tag, response_string)
+
+		return jsonify({}),201
+	else:	
+		return jsonify({}),405
+
 
 
 if __name__ == "__main__":
+
 	app.run(debug=True) 
--- firebase_db.py
+++ firebase_db.py
@@ -1,16 +1,47 @@
 from init_firebase import firebase
+import json
 
 db = firebase.database()
 
 def put_request_data(tag,data):
 
-	db.child(tag).push(data)
+	if tag and data:
+		db.child(tag).push(data)
+	
 
-def get_request_data(data):
+def get_request_data(tag):
 
-	queries = db.child(data).get()
+	if tag:
+		queries = db.child(tag).get()
+		return queries
 
-	return queries
+def update_data(id_tag,tag,data):
+
+	queries = get_request_data(tag).val()
+	response = {}
+
+	for k,v in queries.items():
+		if k == id_tag:
+			response[k] = v
+			break
+
+	if response:
+		response[k]['response'] = data
+		db.child(tag).update(response)
+	else:
+		return None
+
+
+tag = "queries"
+
+id_tag = "-L_6L3Op4SXqvdt2TCAl"
+
+data = "This is nice"
+
+#json_dict = json.dumps(data)
+
+update_data(id_tag,tag,data)
+
 
 '''
 queries = get_request_data("queries")
